c98a4a5ba5a375178590b4c6a105e9c1d2b02071
Willian Giovanini Dei Tosi <104787632+williandeitosi@users.noreply.github.com>
2022-07-21 18:40:45 -0300

    React

---
+++ componentes/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./Header"
+import './App.css';
+
+function App() {
+  return (
+    <Header name="Will" links = {["Sobre","Contato","Local","Login"]}></Header>
+    
+  );
+}
+
+export default App;
