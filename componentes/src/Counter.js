6408d5795168beec3eca8b28fa33356825703996
Willian Giovanini Dei Tosi <104787632+williandeitosi@users.noreply.github.com>
2022-07-25 21:42:08 -0300

    components

--- componentes/src/Counter.js
+++ componentes/src/Counter.js
@@ -13,10 +13,38 @@ class Counter extends React.Component {
 
     //this.setState({ count: this.state.count + 1 }); //forma correta mas pode dar erro
 
-    this.setState((state) => {
-      return { count: state.count + 1 };
-    }); // Assim não da erro pois entra em uma fila e conta os numeros corretamente
+    this.setState(
+      (state) => {
+        return { count: state.count + 1 };
+      },
+      () => {
+        // Assim não da erro pois entra em uma fila e conta os numeros corretamente
+
+        console.log(this.state);
+        localStorage.setItem("state", JSON.stringify(this.state));
+      }
+    );
   }
+
+  /*
+     01) shouldComponentUpdate() {
+    return false;
+    }
+    // esse é mais um ciclo de vida , ele não deixa atulizar o state
+
+
+     02) componentDidMount() {
+    this.setState(JSON.parse(localStorage.getItem("state")));
+
+    // Para carregar as informações que ja foram armazenadas depois que tudo carregar antes
+  }
+
+      03) componentWillUnmount () {
+    // usado para quando for retirar algo da sua aplicação. EX: um itêm do ToDo list que ja foi feito 
+  }
+  */
+
+ 
 
   render() {
     return (
